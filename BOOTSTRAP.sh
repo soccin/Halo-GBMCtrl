59ad126a01230b843270fdf51843935c2ca2f703
Nicholas D. Socci <soccin@mskcc.org>
2025-06-16 20:12:50 -0400

    Added BOOTSTRAP for panel-tme

---
+++ BOOTSTRAP.sh
@@ -0,0 +1,14 @@
+#
+# Study specific variables/functions for setup
+#
+
+#
+# clone the study's repo
+#
+
+#
+#
+#
+
+git submodule add -q -b panel-tme git@github.com:soccin/Halo-GBMCtrl.git config
+
